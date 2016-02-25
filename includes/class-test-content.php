8cb23d39d8b0d4ff888cce5fa085e543f0240c8a
Mike Selander <mikeselander@gmail.com>
2016-02-24 20:26:48 -0700

    Added Video & YouTube methods to test content

--- includes/class-test-content.php
+++ includes/class-test-content.php
@@ -489,4 +489,56 @@ CSSland,
 
 	}
 
+
+	/**
+	 * Youtube.
+	 *
+	 * Returns a random appropriate youtube link.
+	 *
+	 * @return string URL.
+	 */
+	public static function youtube(){
+
+		$links = array(
+			'https://www.youtube.com/watch?v=tntOCGkgt98',
+			'https://www.youtube.com/watch?v=O1KW3ZkLtuo',
+			'https://www.youtube.com/watch?v=G8KpPw303PY',
+			'https://www.youtube.com/watch?v=HxM46vRJMZs',
+			'https://www.youtube.com/watch?v=nRzsgCp60YU',
+			'https://www.youtube.com/watch?v=25OUFtdno8U',
+			'https://www.youtube.com/watch?v=PHAc3_MEjgQ',
+			'https://www.youtube.com/watch?v=9bZkp7q19f0',
+			'https://www.youtube.com/watch?v=_OBlgSz8sSM',
+		);
+
+		return $links[ rand( 0, 8 ) ];
+
+	}
+
+
+	/**
+	 * Vimeo.
+	 *
+	 * Returns a random appropriate vimeo link.
+	 *
+	 * @return string URL.
+	 */
+	public static function vimeo(){
+
+		$links = array(
+			'https://vimeo.com/156161909',
+			'https://vimeo.com/156045670',
+			'https://vimeo.com/144698619',
+			'https://vimeo.com/151799633',
+			'https://vimeo.com/149224063',
+			'https://vimeo.com/154915431',
+			'https://vimeo.com/155404383',
+			'https://vimeo.com/149478317',
+			'https://vimeo.com/154698227',
+		);
+
+		return $links[ rand( 0, 8 ) ];
+
+	}
+
 }
