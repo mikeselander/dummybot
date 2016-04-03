123cdf48456ff25c1f695d8f61bdbe8ec13fff81
Mike Selander <mikeselander@gmail.com>
2016-04-03 09:26:56 +0100

    Added space after creation update line

--- assets/admin.js
+++ assets/admin.js
@@ -57,7 +57,7 @@ jQuery(document).ready(function($) {
 							id = parsed.tid;
 					}
 
-					jQuery( '#status-updates' ).append( innerCount + ':' + test_content.createdStr + ' ' + type + ' ' + id + ': ' + '<a href="' + parsed.link_edit + '">Edit</a> | ' + '<a href="' + parsed.link_view + '">View</a>\n' );
+					jQuery( '#status-updates' ).append( innerCount + ': ' + test_content.createdStr + ' ' + type + ' ' + id + ': ' + '<a href="' + parsed.link_edit + '">Edit</a> | ' + '<a href="' + parsed.link_view + '">View</a>\n' );
 
 					// Re-up our number & scroll to bottom
 					innerCount++;
